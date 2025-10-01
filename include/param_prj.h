33a32dc4959977fbd42828a9bcd0346f7f4f7707
Modellfan <febaschu@aol.com>
2025-10-01 20:27:29 +0200

    Add manual standby override for LVDU

--- include/param_prj.h
+++ include/param_prj.h
@@ -64,6 +64,7 @@
    PARAM_ENTRY(CAT_LVDU, LVDU_12v_low_threshold, "V", 8.0, 13.5, 11.0, 116)               \
    PARAM_ENTRY(CAT_LVDU, LVDU_hv_low_threshold, "V", 100.0, 800.0, 200.0, 117)            \
    PARAM_ENTRY(CAT_LVDU, manual_charge_mode, YESNO, 0, 1, 0, 118)                         \
+   PARAM_ENTRY(CAT_LVDU, manual_standby_mode, YESNO, 0, 1, 0, 121)                        \
                                                                                           \
    PARAM_ENTRY(CAT_LVDU, charge_done_current, "A", 0, 10, 0.5, 119)                       \
                                                                                           \
